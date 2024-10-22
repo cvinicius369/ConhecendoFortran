0cad47f72e0db759162068bf32fb0d35a0c7518d
Caio Vinicius <cvinicius182102@gmail.com>
2024-10-22 12:21:10 -0300

    anotações

--- extensoes.f95
+++ extensoes.f95
@@ -2,6 +2,7 @@
 ! .f e .for são usadas comumente nas versoes do fortran 77
 ! .f90 e .f95 são usadas pelas versoes mais recentes do fortran
 
+! fortran se assemelha bastante com o portugol
 program HelloWorld
     print *, "Hello, World!"
 end program HelloWorld
