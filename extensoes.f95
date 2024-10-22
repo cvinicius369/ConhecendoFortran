64279d4a0c9ccfa1f6a16042ff001589d297d454
Caio Vinicius <cvinicius182102@gmail.com>
2024-10-22 12:19:00 -0300

    primeiros passos

---
+++ extensoes.f95
@@ -0,0 +1,7 @@
+! As extensoes em fortran dependem da versao
+! .f e .for são usadas comumente nas versoes do fortran 77
+! .f90 e .f95 são usadas pelas versoes mais recentes do fortran
+
+program HelloWorld
+    print *, "Hello, World!"
+end program HelloWorld
